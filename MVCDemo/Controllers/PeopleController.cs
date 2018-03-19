8d0c48f80a9546914c0c53e82090357536293596
Salomon Alcidor <salomonalcidor@users.noreply.github.com>
2018-03-19 16:29:37 -0500

    Finished Tutorial

---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Tim", LastName = "Johnson", Age = 32 });
+            people.Add(new PersonModel { FirstName = "Jake", LastName = "Roberts", Age = 46 });
+            people.Add(new PersonModel { FirstName = "Lindsey", LastName = "Connor", Age = 25 });
+
+            return View(people);
+        }
+    }
+}
